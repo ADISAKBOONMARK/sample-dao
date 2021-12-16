7495f0f295f5559740366c3c4841913c1546879d
Adisak Boonmark <adisb603@chare.postbox.in.th>
2021-12-16 21:28:07 +0700

    Add test cases

--- test/mygovernor.test.js
+++ test/mygovernor.test.js
@@ -16,7 +16,17 @@ describe("MyGovernor", async function () {
   
   it("Can create a snapshot", async function () {
     await token.createSnapshot();
-    const snapshot = await token.snapshotList(0);
+    await token.createSnapshot();
+
+    let snapshot = await token.snapshotList(0);
+
+    expect(snapshot.voteStart.toNumber() !== 0).to.equal(true);
+    expect(snapshot.voteEnd.toNumber() !== 0).to.equal(true);
+    expect(snapshot.lastCandidateIndex.toNumber() == 0).to.equal(true);
+    expect(snapshot.executed == false).to.equal(true);
+    expect(snapshot.canceled == false).to.equal(true);
+
+    snapshot = await token.snapshotList(1);
 
     expect(snapshot.voteStart.toNumber() !== 0).to.equal(true);
     expect(snapshot.voteEnd.toNumber() !== 0).to.equal(true);
@@ -28,7 +38,19 @@ describe("MyGovernor", async function () {
   it("Can add a candidate", async function () {
     await token.addCandidate(0);
     await token.addCandidate(0);
-    const snapshot = await token.snapshotList(0);
+
+    let snapshot = await token.snapshotList(0);
+
+    expect(snapshot.voteStart.toNumber() !== 0).to.equal(true);
+    expect(snapshot.voteEnd.toNumber() !== 0).to.equal(true);
+    expect(snapshot.lastCandidateIndex.toNumber() == 2).to.equal(true);
+    expect(snapshot.executed == false).to.equal(true);
+    expect(snapshot.canceled == false).to.equal(true);
+
+    await token.addCandidate(1);
+    await token.addCandidate(1);
+
+    snapshot = await token.snapshotList(1);
 
     expect(snapshot.voteStart.toNumber() !== 0).to.equal(true);
     expect(snapshot.voteEnd.toNumber() !== 0).to.equal(true);
@@ -45,13 +67,88 @@ describe("MyGovernor", async function () {
       token.interface,
       accounts[1]
     );
-    await token.myVoted(0);
+    await token.vote(0, 0)
     const votedReport = await token.votedReport(0);
-    console.log(votedReport);
-    // expect(snapshot.voteStart.toNumber() !== 0).to.equal(true);
-    // expect(snapshot.voteEnd.toNumber() !== 0).to.equal(true);
-    // expect(snapshot.lastCandidateIndex.toNumber() == 2).to.equal(true);
-    // expect(snapshot.executed == false).to.equal(true);
-    // expect(snapshot.canceled == false).to.equal(true);
+
+    expect(votedReport[0].countScore.toNumber() == 2).to.equal(true);
+  });
+
+  it("Should not allow voting again", async function () {
+    let allow = true; 
+
+    try {
+      await token.vote(0, 0);
+    }
+    catch(err) {
+      allow = false;
+    }
+
+    expect(allow == false).to.equal(true);
+  });
+
+  it("Can cancel a snapshot", async function () {
+    await token.cancel(0);
+    const snapshot = await token.snapshotList(0);
+
+    expect(snapshot.canceled == true).to.equal(true);
+  });
+
+  it("Should not allow execute when canceled", async function () {
+    let allow = true; 
+
+    try {
+      await token.execute(0);
+    }
+    catch(err) {
+      allow = false;
+    }
+
+    expect(allow == false).to.equal(true);
+  });
+
+  it("Should not allow vote when canceled", async function () {
+    let allow = true; 
+
+    try {
+      await token.vote(0,0);
+    }
+    catch(err) {
+      allow = false;
+    }
+
+    expect(allow == false).to.equal(true);
+  });
+
+  it("Can execute a snapshot", async function () {
+    await token.execute(1);
+    const snapshot = await token.snapshotList(0);
+
+    expect(snapshot.canceled == true).to.equal(true);
+  });
+
+  it("Should not allow cancel when executed", async function () {
+    let allow = true; 
+
+    try {
+      await token.cancel(1);
+    }
+    catch(err) {
+      allow = false;
+    }
+
+    expect(allow == false).to.equal(true);
+  });
+
+  it("Should not allow vote when executed", async function () {
+    let allow = true; 
+
+    try {
+      await token.vote(1,0);
+    }
+    catch(err) {
+      allow = false;
+    }
+
+    expect(allow == false).to.equal(true);
   });
 });

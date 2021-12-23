d686dd88d8ad9f600745cf203948c49cdcd07aa1
Adisak Boonmark <adisb603@chare.postbox.in.th>
2021-12-23 18:25:03 +0700

    Add func for member

--- contracts/MyGovernor.sol
+++ contracts/MyGovernor.sol
@@ -14,7 +14,7 @@ contract MyGovernor is Ownable{
         Counters.Counter countScore;
     }
 
-    struct Governor {
+    struct Vote {
         bool voted;
         uint256 candidateIndex;
     }
@@ -22,38 +22,38 @@ contract MyGovernor is Ownable{
     struct Snapshot {
         uint256 voteStart;
         uint256 voteEnd;
+        uint256 location;
         Counters.Counter lastCandidateIndex;
         bool canceled;
-        mapping (address => Governor) governorList;
+        mapping (address => Vote) voteList;
         mapping (uint256 => Candidate) candidateList;
     }
 
     struct Snap {
         uint256 voteStart;
         uint256 voteEnd;
+        uint256 location;
         uint256 lastCandidateIndex;
         bool canceled;
     }
 
     mapping(uint256 => Snapshot) private snapshotList;
 
+    mapping(address => uint256) private location;
+
     Counters.Counter private lastSnapshotIndex;
 
     constructor(){}
     
-    function getSnapshot(uint256 _snapshotIndex) public view returns(Snap memory) {
-        Snap memory snap = Snap({
-            voteStart: snapshotList[_snapshotIndex].voteStart,
-            voteEnd: snapshotList[_snapshotIndex].voteEnd,
-            lastCandidateIndex: snapshotList[_snapshotIndex].lastCandidateIndex.current(),
-            canceled: snapshotList[_snapshotIndex].canceled
-        });
-        return snap;
-    }
-
     function isOpenVote(uint256 snapshotIndex) private view returns(bool result) {
         if(snapshotList[snapshotIndex].voteStart <= block.timestamp && 
         snapshotList[snapshotIndex].voteEnd > block.timestamp) {
+            result = true;
+        }
+    }
+
+    function isLocation(uint256 snapshotIndex, address account) private view returns(bool result) {
+        if(location[account] == snapshotList[snapshotIndex].location) {
             result = true;
         }
     }
@@ -74,16 +74,27 @@ contract MyGovernor is Ownable{
         }
     }
 
-    function isDuplicateVote(uint256 snapshotIndex, address sender) private view returns(bool result) {
-        result = snapshotList[snapshotIndex].governorList[sender].voted;
+    function isDuplicateVote(uint256 snapshotIndex, address account) private view returns(bool result) {
+        result = snapshotList[snapshotIndex].voteList[account].voted;
     }
 
-    function createSnapshot(uint256 _voteStart, uint256 _voteEnd) public onlyOwner {
+    function register(uint256 _location) public {
+        //TODO: Verify with ERC20 MFW.
+        location[_msgSender()] = _location;
+    }
+
+    function setLocation(uint256 _location) public {
+        //TODO: Verify only member.
+        location[_msgSender()] = _location;
+    }
+
+    function createSnapshot(uint256 _voteStart, uint256 _voteEnd, uint256 _location) public onlyOwner {
         require(_voteStart >= block.timestamp, "50001");
         require(_voteStart < _voteEnd, "50002");
 
         snapshotList[lastSnapshotIndex.current()].voteStart = _voteStart;
         snapshotList[lastSnapshotIndex.current()].voteEnd = _voteEnd;
+        snapshotList[lastSnapshotIndex.current()].location = _location;
         lastSnapshotIndex.increment();
     }
 
@@ -101,12 +112,13 @@ contract MyGovernor is Ownable{
         require(isFoundSnapshot(_snapshotIndex), "50003");
         require(isFoundCandidate(_snapshotIndex,_candidateIndex), "50006");
         require(isOpenVote(_snapshotIndex), "50007");
+        require(isLocation(_snapshotIndex, _msgSender()), "50009");
         require(!isCancelVote(_snapshotIndex), "50005");
-        require(!isDuplicateVote(_snapshotIndex, msg.sender), "50008");
+        require(!isDuplicateVote(_snapshotIndex, _msgSender()), "50008");
 
         snapshotList[_snapshotIndex].candidateList[_candidateIndex].countScore.increment();
-        snapshotList[_snapshotIndex].governorList[msg.sender].candidateIndex = _candidateIndex;
-        snapshotList[_snapshotIndex].governorList[msg.sender].voted = true;
+        snapshotList[_snapshotIndex].voteList[_msgSender()].candidateIndex = _candidateIndex;
+        snapshotList[_snapshotIndex].voteList[_msgSender()].voted = true;
     }
 
     function cancel(uint256 _snapshotIndex) public onlyOwner {
@@ -117,6 +129,17 @@ contract MyGovernor is Ownable{
         snapshotList[_snapshotIndex].canceled = true;
     }
 
+    function getSnapshot(uint256 _snapshotIndex) public view returns(Snap memory) {
+        Snap memory snap = Snap({
+            voteStart: snapshotList[_snapshotIndex].voteStart,
+            voteEnd: snapshotList[_snapshotIndex].voteEnd,
+            location: snapshotList[_snapshotIndex].location,
+            lastCandidateIndex: snapshotList[_snapshotIndex].lastCandidateIndex.current(),
+            canceled: snapshotList[_snapshotIndex].canceled
+        });
+        return snap;
+    }
+    
     function voteReport(uint256 _snapshotIndex) public view returns(Candidate[] memory) {
         Candidate[] memory result = new Candidate[](snapshotList[_snapshotIndex].lastCandidateIndex.current());
 
@@ -131,7 +154,11 @@ contract MyGovernor is Ownable{
         return snapshotList[_snapshotIndex].candidateList[_candidateIndex];
     }
 
-    function myVote(uint256 _snapshotIndex) public view returns(Governor memory) {
-        return snapshotList[_snapshotIndex].governorList[msg.sender];
+    function myVote(uint256 _snapshotIndex) public view returns(Vote memory) {
+        return snapshotList[_snapshotIndex].voteList[_msgSender()];
+    }
+
+    function myLocation() public view returns(uint256) {
+        return location[_msgSender()];
     }
 }
